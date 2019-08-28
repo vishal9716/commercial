841556759f339ce228c50447f61ffba7441249dc
parul9goyal@gmail.com <parul9goyal@gmail.com>
2019-08-28 18:17:22 +0530

    commit for purchase order

--- application/views/common/bottom.php
+++ application/views/common/bottom.php
@@ -1,15 +1,15 @@
 </div>
     <!-- /#wrapper -->
 <!-- datatable -->
-<script src="<?php echo base_url(); ?>vendor/datatables/js/jquery.dataTables.min.js"></script>
+
 <!-- end datatable -->
   <script src="<?php echo base_url(); ?>vendor/jquery/jquery.min.js"></script>
     <!-- jQuery -->
-    <script src="<?php echo base_url(); ?>vendor/jquery/jquery.min.js"></script>
+    
 
     <!-- Bootstrap Core JavaScript -->
     <script src="<?php echo base_url(); ?>vendor/bootstrap/js/bootstrap.min.js"></script>
-
+<script src="<?php echo base_url(); ?>vendor/datatables/js/jquery.dataTables.min.js"></script>
     <!-- Metis Menu Plugin JavaScript -->
     <script src="<?php echo base_url(); ?>vendor/metisMenu/metisMenu.min.js"></script>
 
